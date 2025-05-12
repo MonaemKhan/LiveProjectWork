0529a50a3764480a659babb5dd67355bb87b9129
M.A Monaem Khan <khanmonaem07@gmail.com>
2025-05-12 18:01:55 +0600

    file and folder added

---
+++ Scripts/Administrator/Bangladesh Meta Data/DivisionList.sql
@@ -0,0 +1,21 @@
+CREATE TABLE geo.divisions (
+  id INT primary key NOT NULL,
+  name nvarchar(100) NOT NULL,
+  country_id INT not null,
+  bn_name nvarchar(100) NOT NULL,
+  url nvarchar(100) NOT NULL,
+  CONSTRAINT FK_Divisions_Country FOREIGN KEY (country_id)
+        REFERENCES geo.countries(Id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+INSERT INTO geo.divisions (id, name, bn_name, url,country_id) VALUES
+(1, 'Chattagram', 'চট্টগ্রাম', 'www.chittagongdiv.gov.bd',14),
+(2, 'Rajshahi', 'রাজশাহী', 'www.rajshahidiv.gov.bd',14),
+(3, 'Khulna', 'খুলনা', 'www.khulnadiv.gov.bd',14),
+(4, 'Barisal', 'বরিশাল', 'www.barisaldiv.gov.bd',14),
+(5, 'Sylhet', 'সিলেট', 'www.sylhetdiv.gov.bd',14),
+(6, 'Dhaka', 'ঢাকা', 'www.dhakadiv.gov.bd',14),
+(7, 'Rangpur', 'রংপুর', 'www.rangpurdiv.gov.bd',14),
+(8, 'Mymensingh', 'ময়মনসিংহ', 'www.mymensinghdiv.gov.bd',14);
