057f672a1b5965e9f9310836ef5aa8b657e5d51c
M.A Monaem Khan <khanmonaem07@gmail.com>
2025-05-13 15:42:44 +0600

    update gitignore for .bak file

--- Database_Backup/Database Backup Script office.sql
+++ Database_Backup/Database Backup Script office.sql
@@ -1,5 +1,5 @@
 BACKUP DATABASE Administrator
-TO DISK = 'D:\GIT DIRECTORIES\LiveProjectWork\Database_Backup\Administration\Administrator_Offfice_13May2025_0336PM.bak'
+TO DISK = 'D:\GIT DIRECTORIES\LiveProjectWork\Database_Backup\Administration\Office\Administrator_13May2025_0340PM.bak'
 WITH FORMAT,
      MEDIANAME = 'Administrator',
      NAME = 'Full Backup of Administrator';
--- Database_Backup/Database Backup Script.sql
+++ Database_Backup/Database Backup Script.sql
@@ -1,5 +1,5 @@
 BACKUP DATABASE Administrator
-TO DISK = 'D:\Monaem Personal\GIT REPO\LiveProjectWork\Database_Backup\Administration\Administrator_13May2025_0329AM.bak'
+TO DISK = 'D:\Monaem Personal\GIT REPO\LiveProjectWork\Database_Backup\Administration\Home\Administrator_13May2025_0329AM.bak'
 WITH FORMAT,
      MEDIANAME = 'Administrator',
      NAME = 'Full Backup of Administrator';
