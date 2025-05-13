eafddfed54c17af967846d92d143638ffb4df6c3
M.A Monaem Khan <khanmonaem07@gmail.com>
2025-05-13 15:36:45 +0600

    update office script

--- Database_Backup/Database Backup Script office.sql
+++ Database_Backup/Database Backup Script office.sql
@@ -1,5 +1,5 @@
 BACKUP DATABASE Administrator
-TO DISK = 'D:\GIT DIRECTORIES\LiveProjectWork\Database_Backup\Administration\PC_Office\Administrator_13May2025_0329AM.bak'
+TO DISK = 'D:\GIT DIRECTORIES\LiveProjectWork\Database_Backup\Administration\Administrator_Offfice_13May2025_0336PM.bak'
 WITH FORMAT,
      MEDIANAME = 'Administrator',
      NAME = 'Full Backup of Administrator';
