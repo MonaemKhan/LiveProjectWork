6d4c5cff2b961ff22958de39d362aae8f6a55172
M.A. Monaem Khan <khanmonaem07@gmail.com>
2025-05-14 08:26:40 +0600

    update database

--- Database_Backup/Database Backup Script.sql
+++ Database_Backup/Database Backup Script.sql
@@ -1,5 +1,5 @@
 BACKUP DATABASE Administrator
-TO DISK = 'D:\Monaem Personal\GIT REPO\LiveProjectWork\Database_Backup\Administration\Home\Administrator_13May2025_0329AM.bak'
+TO DISK = 'D:\Monaem Personal\GIT REPO\LiveProjectWork\Database_Backup\Administration\Home\Administrator_14May2025_0826AM.bak'
 WITH FORMAT,
      MEDIANAME = 'Administrator',
      NAME = 'Full Backup of Administrator';
